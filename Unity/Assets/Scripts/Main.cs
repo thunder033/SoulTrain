820b6602b5ead7f9a5d7f7b4d8f121642c65738e
Greg Rozmarynowycz <crashtest1@live.com>
2016-02-08 17:56:10 -0500

    switch to force text asset serialization and visible meta files

---
+++ Unity/Assets/Scripts/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 position = new Vector3();
+        GameObject trainCar = (GameObject)Instantiate(Resources.Load("/Prefabs/TrainCar"));
+        trainCar.transform.position = position;
+        trainCar.transform.Rotate(new Vector3(0, 0, 90));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
