2a041f93d130c98c4dc15a7939a3416f96c01fd7
Greg Rozmarynowycz <crashtest1@live.com>
2016-02-11 23:23:41 -0500

    make some train cars

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -8,9 +8,9 @@ public class Main : MonoBehaviour
     void Start()
     {
         Vector3 position = new Vector3();
-        GameObject trainCar = (GameObject)Instantiate(Resources.Load("/Prefabs/TrainCar"));
+        GameObject trainCar = (GameObject)Instantiate(Resources.Load("Prefabs/TrainCar") as GameObject);
         trainCar.transform.position = position;
-        trainCar.transform.Rotate(new Vector3(0, 0, 90));
+        trainCar.transform.Rotate(new Vector3(0, 45, 0));
     }
 
     // Update is called once per frame
