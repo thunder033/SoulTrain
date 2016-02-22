c35fae3c01b6b667f21351215d23db34ce7022ea
Greg Rozmarynowycz <crashtest1@live.com>
2016-02-22 01:19:22 -0500

    experimental loading code

--- Unity/Assets/Scripts/StoryBuilder.cs
+++ Unity/Assets/Scripts/StoryBuilder.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using SimpleJSON;
+using System.Collections.Generic;
 
-public class StoryBuilder : MonoBehaviour {
+public static class StoryBuilder {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    static private Type[] storyElements = new Type[] { typeof(DialogLine) };
+
+	public static void LoadStoryElements()
+    {
+        foreach(Type element in storyElements)
+        {
+
+        }
+    }
+}
+
+public abstract class DataLoader<T>
+{
+
+    public abstract T GetByID(string id);
+
+    public abstract T FromJSON(JSONNode data);
+}
+
+public class DialogLoader : DataLoader<DialogLine>
+{
+    private Dictionary<string, DialogLine> dialogLines;
+
+    public override DialogLine GetByID(string id)
+    {
+        return dialogLines[id];
+    }
+
+    public override DialogLine FromJSON(JSONNode data)
+    {
+        return new DialogLine(data);
+    }
 }
