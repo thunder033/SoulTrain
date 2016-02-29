43ecf9a5aa45e39fcfec486c289eee489fbce025
Greg Rozmarynowycz <crashtest1@live.com>
2016-02-29 01:34:23 -0500

    display dialog on gui

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    public Canvas canvas;
+
+    DialogLine line;
 
     // Use this for initialization
     void Start()
@@ -12,12 +16,21 @@ public class Main : MonoBehaviour
         trainCar.transform.Rotate(new Vector3(0, 45, 0));
 
         Story.LoadStoryElements();
+
+        line = Story.GetElementById<DialogLine>("0");
+        string text = line.text;
+        GameObject.Find("DialogBox").GetComponent<Text>().text = text;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            line = line.GetResponses()[0];
+            string text = line.text;
+            GameObject.Find("DialogBox").GetComponent<Text>().text = text;
+        }
     }
 
     public bool LoadDialogLines()
