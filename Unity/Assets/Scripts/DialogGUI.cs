4e3d0f576414bffb718acf9e731e97bb663d4bcc
Greg Rozmarynowycz <crashtest1@live.com>
2016-03-01 00:21:06 -0500

    add soul, NPC, and Dialog GUI scripts

---
+++ Unity/Assets/Scripts/DialogGUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+[RequireComponent (typeof (Soul))]
+public class DialogGUI : MonoBehaviour {
+    Soul soul;
+    DialogLine currentLine;
+
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+        soul = GetComponent<Soul>();
+        soul.HearResponse = new Conversation.ResponseHandler(Respond);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            soul.Speak(currentLine.GetResponses()[0]);
+        }
+    }
+
+    public virtual void Respond(Conversation convo)
+    {
+        currentLine = convo.CurrentLine;
+        GameObject.Find("DialogBox").GetComponent<Text>().text = currentLine.speaker.name + ": " + currentLine.text;
+    }
+}
---
+++ Unity/Assets/Scripts/NPC.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Soul))]
+public class NPC : MonoBehaviour
+{
+    Soul soul;
+
+    // Use this for initialization
+    void Start()
+    {
+        soul = GetComponent<Soul>();
+        soul.HearResponse = new Conversation.ResponseHandler(Respond);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Respond(Conversation convo)
+    {
+
+    }
+}
