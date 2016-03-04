624d813fc2fb413f8cc5e4885c2e53f0ec867db2
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-03-04 11:54:29 -0500

    player movement

---
+++ Unity/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    Vector3 velocity;
+
+    public float speed;
+    float maxSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = new Vector3();
+
+        velocity.y = 0;
+        velocity.x += Input.GetAxis("Horizontal") * Time.deltaTime;
+        velocity.z += Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.position += velocity;
+        float heading = Mathf.Atan2(velocity.x, velocity.z) * Mathf.Rad2Deg;
+
+        if(velocity.magnitude > 0)
+        {
+            transform.localEulerAngles = new Vector3(0, heading, 0);
+        }
+        
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, heading, 0), 5 * Time.deltaTime);
+    }
+}
