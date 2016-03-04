d2db4eac214657503aca84666275763e96fea6b0
Greg Rozmarynowycz <crashtest1@live.com>
2016-03-03 19:38:02 -0500

    prevent cs project to reverting to c# 4

---
+++ Unity/Assets/Editor/LangRemover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEditor;
+using SyntaxTree.VisualStudio.Unity.Bridge;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class LangRemover
+{
+
+    static LangRemover()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) =>
+        {
+            // This will remove the language restriction to C# 4
+            return content.Replace("<LangVersion Condition=\" '$(VisualStudioVersion)' != '10.0' \">4</LangVersion>", string.Empty);
+        };
+    }
+}
+    
