fb71f6497ebd533d140619bede734d967f1b8f1b
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-03-11 10:51:40 -0500

    display game saves on menu

--- Unity/Assets/Scripts/UI/LoadMenu.cs
+++ Unity/Assets/Scripts/UI/LoadMenu.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LoadMenu : MonoBehaviour {
 
+    public Canvas loadMenu;
+    public GameObject savePanelPrefab;
+    public string titleFieldName;
+    public string dateFieldName;
+
+    string selectedGame;
+
 	// Use this for initialization
 	void Start () {
-	
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject savePanel = Instantiate(savePanelPrefab);
+            savePanel.transform.Find(titleFieldName).GetComponent<Text>().text = Game.Saves[i].name;
+            savePanel.transform.SetParent(loadMenu.transform);
+            savePanel.GetComponent<Image>().rectTransform.anchoredPosition = new Vector2(-50 + 50 * i, 0);
+        }
 	}
 	
 	// Update is called once per frame
@@ -16,5 +30,10 @@ public class LoadMenu : MonoBehaviour {
     public void LoadMainMenu()
     {
         Application.LoadLevel("Menu");
+    }
+
+    public void LoadGame()
+    {
+
     }
 }
