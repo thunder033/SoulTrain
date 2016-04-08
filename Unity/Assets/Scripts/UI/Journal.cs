d4c4fccaf4aab041ec2d26d211a687793d63f492
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-04-08 10:53:49 -0400

    open journal, define sections

---
+++ Unity/Assets/Scripts/UI/Journal.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Journal : MonoBehaviour {
+
+    enum Section
+    {
+        Mysteries,
+        PauseMenu
+    }
+
+    Dictionary<Section, GameObject> sections;
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+
+        sections = new Dictionary<Section, GameObject>();
+        sections.Add(Section.Mysteries, GameObject.Find(Section.Mysteries.ToString()));
+        sections.Add(Section.PauseMenu, GameObject.Find(Section.PauseMenu.ToString()));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+             Close();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            OpenSection(Section.Mysteries);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            OpenSection(Section.PauseMenu);
+	}
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OpenSection(Section name)
+    {
+        foreach(GameObject section in sections.Values)
+        {
+            section.SetActive(false);
+        }
+
+        sections[name].SetActive(true);
+    }
+}
--- Unity/Assets/Scripts/UI/TrainLoader.cs
+++ Unity/Assets/Scripts/UI/TrainLoader.cs
@@ -4,8 +4,21 @@ using System.Collections;
 
 public class TrainLoader : SceneLoader
 {
+    Journal journal;
     public void Awake()
     {
         Story.LoadStoryElements();
+    }
+
+    public new void Start()
+    {
+        base.Start();
+        journal = GameObject.Find("JournalGUI").GetComponent<Journal>();
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+            journal.Open();
     }
 }
