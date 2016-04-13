67c0a9c3bee8a8682995d4af396db643534f3857
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-04-13 10:52:32 -0400

    set minimum Z position on player so they are locked in the car

--- Unity/Assets/Scripts/UI/PlayerControl.cs
+++ Unity/Assets/Scripts/UI/PlayerControl.cs
@@ -5,6 +5,7 @@ public class PlayerControl : MonoBehaviour {
 
     public Vector3 inputVelocity {get; private set;}
     public float speed = 1;
+    private float minZ = -0.75f;
 	
 	void Update () {
         //get the player inputted velocity
@@ -18,6 +19,11 @@ public class PlayerControl : MonoBehaviour {
             float heading = Mathf.Atan2(inputVelocity.x, inputVelocity.z) * Mathf.Rad2Deg;
             //rotate the avatar to match their velocity
             transform.localEulerAngles = Vector3.up * heading;
+        }
+
+        if(transform.position.z < minZ)
+        {
+            transform.position += new Vector3(0, 0, minZ - transform.position.z);
         }
     }
 }
